142116dff3e9230498fcf6ba3a4a3f9a8846e10a
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-09-28 11:59:41 -0700

    Update Cut-the-ropes.js

--- Cut-the-ropes.js
+++ Cut-the-ropes.js
@@ -1,6 +1,6 @@
 /*
- You are given N ropes, where the length of each rope is a positive integer. 
- At each step, you have to reduce all the ropes by the length of the smallest rope.
+You are given N ropes, where the length of each rope is a positive integer. 
+At each step, you have to reduce all the ropes by the length of the smallest rope.
 
 The step will be repeated until no ropes are left. Given the length of N ropes, 
 print the number of ropes that are left before each step.
